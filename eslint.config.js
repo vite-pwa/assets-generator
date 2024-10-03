b2f03ac1825fbdf2f2b94f2048f5a369e12ccb8a
userquin <userquin@gmail.com>
2024-10-03 11:28:43 +0200

    chore: cleanup eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,9 @@
 import antfu from '@antfu/eslint-config'
 
-export default await antfu(
-  {
-    ignores: [
-      'test/cases',
-    ],
-  },
+export default antfu(
   {
     rules: {
-      'node/prefer-global/process': 'off',
-      'ts/no-this-alias': 'off',
       'no-restricted-globals': 'off',
-      'node/handle-callback-err': 'off',
     },
   },
 )
